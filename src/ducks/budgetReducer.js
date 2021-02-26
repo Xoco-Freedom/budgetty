0d66c58d495520a41529eca48bf59f9c35f6faaa
rachelseaborn <raseaborn@gmail.com>
2021-02-25 17:56:34 -0700

    updates

--- src/ducks/budgetReducer.js
+++ src/ducks/budgetReducer.js
@@ -6,7 +6,25 @@ const initialState = {
   loading: false,
 };
 
+const REQUEST_BUDGET_DATA = "REQUEST_BUDGET_DATA";
+
+export const requestBudgetData = () => {
+  let data = axios.get("/api/budget-data").then((res) => res.data);
+  return {
+    type: REQUEST_BUDGET_DATA,
+    payload: data,
+  };
+};
+
 export default function reducer(state, action) {
   state = initialState;
-  return state;
+
+  switch (action.type) {
+    case REQUEST_BUDGET_DATA + "_PENDING":
+      return { ...state, loading: true };
+    case REQUEST_BUDGET_DATA + "_FULFILLED":
+      return { ...state, ...action.payload, loading: false };
+    default:
+      return state;
+  }
 }
